9aed035427ff148230453707431d52d98f001341
Alexandr Kolodkin <alexandr.kolodkin@gmail.com>
2020-04-18 14:30:24 +0300

    Обновлен номер ревизии

--- build.bat
+++ build.bat
@@ -8,4 +8,4 @@ xcopy "ModAlarm\bin\Release\ModAlarm.dll"  "ScadaServer\Mod" /F /Y
 xcopy "ModAlarm\bin\Release\NAudio.dll"    "ScadaServer\Mod" /F /Y
 xcopy "ModAlarm\ModAlarm.en-Gb.xml"        "ScadaServer\Lang" /F /Y
 
-7z a "ModAlarm 1.2.5.zip" ".\ScadaServer\*"
+7z a "ModAlarm-1.2.5.zip" ".\ScadaServer\*"
